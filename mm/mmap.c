41f6e61b0e978ff74b67c7d6e29d25d2069d4940
Al Viro <viro@zeniv.linux.org.uk>
2012-05-30 17:13:15 -0400

    move security_mmap_addr() to saner place
    it really should be done by get_unmapped_area(); that cuts down on
    the amount of callers considerably and it's the right place for
    that stuff anyway.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1177,7 +1177,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	struct mm_struct * mm = current->mm;
 	struct inode *inode;
 	vm_flags_t vm_flags;
-	int error;
 
 	/*
 	 * Does the application expect PROT_READ to imply PROT_EXEC?
@@ -1301,10 +1300,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			return -EINVAL;
 		}
 	}
-
-	error = security_mmap_addr(addr);
-	if (error)
-		return error;
 
 	return mmap_region(file, addr, len, flags, vm_flags, pgoff);
 }
@@ -1995,7 +1990,9 @@ get_unmapped_area(struct file *file, unsigned long addr, unsigned long len,
 	if (addr & ~PAGE_MASK)
 		return -EINVAL;
 
-	return arch_rebalance_pgtables(addr, len);
+	addr = arch_rebalance_pgtables(addr, len);
+	error = security_mmap_addr(addr);
+	return error ? error : addr;
 }
 
 EXPORT_SYMBOL(get_unmapped_area);
@@ -2646,10 +2643,6 @@ static unsigned long do_brk(unsigned long addr, unsigned long len)
 	if (!len)
 		return addr;
 
-	error = security_mmap_addr(addr);
-	if (error)
-		return error;
-
 	flags = VM_DATA_DEFAULT_FLAGS | VM_ACCOUNT | mm->def_flags;
 	uksm_vm_flags_mod(&flags);
 
@@ -3010,10 +3003,6 @@ int install_special_mapping(struct mm_struct *mm,
 
 	vma->vm_ops = &special_mapping_vmops;
 	vma->vm_private_data = pages;
-
-	ret = security_mmap_addr(vma->vm_start);
-	if (ret)
-		goto out;
 
 	ret = insert_vm_struct(mm, vma);
 	if (ret)
