13e7cfcc76c6a6299e882a464c3e68a9aaeb5d47
Lukas Czerner <lczerner@redhat.com>
2012-09-26 22:21:21 -0400

    ext4: enable FITRIM ioctl on bigalloc file system
    With a minor tweaks regarding minimum extent size to discard and
    discarded bytes reporting the FITRIM can be enabled on bigalloc file
    system and it works without any problem.
    
    This patch fixes minlen handling and discarded bytes reporting to
    take into consideration bigalloc enabled file systems and finally
    removes the restriction and allow FITRIM to be used on file system with
    bigalloc feature enabled.
    
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -421,13 +421,6 @@ resizefs_out:
 		if (!blk_queue_discard(q))
 			return -EOPNOTSUPP;
 
-		if (EXT4_HAS_RO_COMPAT_FEATURE(sb,
-			       EXT4_FEATURE_RO_COMPAT_BIGALLOC)) {
-			ext4_msg(sb, KERN_ERR,
-				 "FITRIM not supported with bigalloc");
-			return -EOPNOTSUPP;
-		}
-
 		if ((flags & BLKDEV_DISCARD_SECURE) && !blk_queue_secdiscard(q))
 			return -EOPNOTSUPP;
 		if (copy_from_user(&range, (struct fstrim_range __user *)arg,
