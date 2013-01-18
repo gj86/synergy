889a20902f9cfe5ef6920cf82c7bbd588ee9fc73
Tejun Heo <tj@kernel.org>
2013-01-18 14:05:56 -0800

    init, block: try to load default elevator module early during boot
    This patch adds default module loading and uses it to load the default
    block elevator.  During boot, it's called right after initramfs or
    initrd is made available and right before control is passed to
    userland.  This ensures that as long as the modules are available in
    the usual places in initramfs, initrd or the root filesystem, the
    default modules are loaded as soon as possible.
    
    This will replace the on-demand elevator module loading from elevator
    init path.
    
    v2: Fixed build breakage when !CONFIG_BLOCK.  Reported by kbuild test
        robot.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Alex Riesen <raa.lkml@gmail.com>
    Cc: Fengguang We <fengguang.wu@intel.com>

--- init/main.c
+++ init/main.c
@@ -73,6 +73,9 @@
 #endif
 #include <linux/random.h>
 #include <linux/sched_clock.h>
+#include <linux/ptrace.h>
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -953,6 +956,17 @@ static void __init do_pre_smp_initcalls(void)
 		do_one_initcall(*fn);
 }
 
+/*
+ * This function requests modules which should be loaded by default and is
+ * called twice right after initrd is mounted and right before init is
+ * exec'd.  If such modules are on either initrd or rootfs, they will be
+ * loaded before control is passed to userland.
+ */
+void __init load_default_modules(void)
+{
+	load_default_elevator_module();
+}
+
 static void run_init_process(const char *init_filename)
 {
 	argv_init[0] = init_filename;
@@ -1096,4 +1110,7 @@ static int __init kernel_init(void * unused)
 
 	init_post();
 	return 0;
+
+	/* rootfs is available now, try loading default modules */
+	load_default_modules();
 }
