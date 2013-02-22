8f0d6e43b0436e3ea31a99719c339aa870c476a1
Lianwei Wang <lian-wei.wang@motorola.com>
2013-02-22 11:39:18 +0800

    cpufreq: Move get_cpu_idle_time() to cpufreq.c
    cpufreq: Move get_cpu_idle_time() to cpufreq.c
    
    Governors other than ondemand and conservative can also use
    get_cpu_idle_time() and they aren't required to compile
    cpufreq_governor.c. So, move these independent routines to
    cpufreq.c instead.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Git-commit: 72a4ce340a7ebf39e1c6fdc8f5feb4f974d6c635
    Git-repo:
    git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [junjiew@codeaurora.org: update non-upstream files]
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    
    Change-Id: I3227454599250011a19a752c962e3cacb1fb0301
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- drivers/cpufreq/cpufreq_nightmare.c
+++ drivers/cpufreq/cpufreq_nightmare.c
@@ -617,40 +617,6 @@ static struct attribute_group nightmare_attr_group = {
 
 /************************** sysfs end ************************/
 
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
-
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-		*wall = jiffies_to_usecs(cur_wall_time);
-
-	return jiffies_to_usecs(idle_time);
-}
-
-static inline u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
-
-	if (idle_time == -1ULL)
-		return get_cpu_idle_time_jiffy(cpu, wall);
-	else if (!io_busy)
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
-
 static void nightmare_check_cpu(struct cpufreq_nightmare_cpuinfo *this_nightmare_cpuinfo)
 {
 	struct cpufreq_policy *cpu_policy;
