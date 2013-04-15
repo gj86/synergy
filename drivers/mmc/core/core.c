99cbdcff38503e5d75a23bf9f2ec4cad4635a3ab
Adrian Hunter <adrian.hunter@intel.com>
2013-04-15 11:27:25 -0400

    mmc: core: fix init controller performance regression, updated patch
    Add MMC_CAP2_NO_PRESCAN_POWERUP to sdhci-pci.c also, use mmc_power_off()
    for MMC_CAP2_NO_PRESCAN_POWERUP.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    [cjb: previously applied v1 of this patch instead of v4]
    Signed-off-by: Chris Ball <cjb@laptop.org>
    
    # Conflicts:
    #	drivers/mmc/core/core.c
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3415,8 +3415,9 @@ void mmc_start_host(struct mmc_host *host)
 {
 	host->f_init = max(freqs[0], host->f_min);
 	host->rescan_disable = 0;
-
-	if (!(host->caps2 & MMC_CAP2_NO_PRESCAN_POWERUP))
+	if (host->caps2 & MMC_CAP2_NO_PRESCAN_POWERUP)
+		mmc_power_off(host);
+	else
 		mmc_power_up(host);
 
 #if defined(CONFIG_MACH_HLTESKT)||defined(CONFIG_MACH_HLTEKTT)||defined(CONFIG_MACH_HLTELGT)\
