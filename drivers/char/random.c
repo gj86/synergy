5ae3e784fe10a2831827f51f26017b47b90b2ab2
Theodore Ts'o <tytso@mit.edu>
2013-11-03 06:54:51 -0500

    random: printk notifications for urandom pool initialization
    Print a notification to the console when the nonblocking pool is
    initialized.  Also printk a warning when a process tries reading from
    /dev/urandom before it is fully initialized.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    
    (cherry picked from commit 08db4bd72e573d257754241b0da5adc0e1ca020e)

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -663,6 +663,9 @@ retry:
 	r->entropy_total += nbits;
 	if (!r->initialized && nbits > 0) {
 		if (r->entropy_total > 128) {
+			if (r == &nonblocking_pool)
+				pr_notice("random: %s pool is initialized\n",
+					  r->name);
 			r->initialized = 1;
 			r->entropy_total = 0;
 		}
@@ -1347,17 +1350,27 @@ http://lwn.net/Articles/489734/ - WIP
 static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
+	int ret;
+
 #ifdef CONFIG_CRYPTO_FIPS
 	int cc_flag = get_cc_mode_state();
 	if ((cc_flag & FLAG_FORCE_USE_RANDOM_DEV) == FLAG_FORCE_USE_RANDOM_DEV)
 		return random_read(file, buf, nbytes, ppos);
-	else
+	else {
 #endif
-	int ret = extract_entropy_user(&nonblocking_pool, buf, nbytes);
+	int ret;
+
+	if (unlikely(nonblocking_pool.initialized == 0))
+		printk_once(KERN_NOTICE "random: %s urandom read "
+			    "with %d bits of entropy available\n",
+			    current->comm, nonblocking_pool.entropy_total);
+
+	ret = extract_entropy_user(&nonblocking_pool, buf, nbytes);
 
 	trace_urandom_read(8 * nbytes, ENTROPY_BITS(&nonblocking_pool),
 			   ENTROPY_BITS(&input_pool));
 	return ret;
+	}
 }
 
 static unsigned int
