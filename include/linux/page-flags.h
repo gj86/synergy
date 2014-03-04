ec73ef6e108ff7c8c45e11b8fa32816c084effa0
Pradeep Sawlani <sawlani@amazon.com>
2014-03-03 18:48:53 -0800

    ksm: check and skip page, if it is already scanned
    On system like Android where most of the process are forked
    from parent w/o execve, KSM can scan same page multiple times
    in one scan cycle. There is no advantage in scanning same page
    multiple times for merging. During testing with Android, it was
    observed around 60% pages are skipped for each scan cycle.
    
    Signed-off-by: Pradeep Sawlani <sawlani@amazon.com>
    [neo: Enabled by default.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    # Conflicts:
    #	include/linux/page-flags.h
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -116,6 +116,8 @@ enum pageflags {
 #endif
 #ifdef CONFIG_SDP
 	PG_sensitive,
+#ifdef CONFIG_KSM_CHECK_PAGE
+	PG_ksm_scan0,           /* page has been scanned by even KSM cycle */
 #endif
 #ifdef CONFIG_ZCACHE
 	PG_was_active,
@@ -125,6 +127,10 @@ enum pageflags {
 	PG_cma,			/* page in CMA area */
 #endif
 
+#ifdef CONFIG_KSM_CHECK_PAGE
+	/* page has been scanned by odd KSM cycle */
+        PG_ksm_scan1 = PG_owner_priv_1,
+#endif
 	/* Filesystems */
 	PG_checked = PG_owner_priv_1,
 
@@ -224,6 +230,10 @@ PAGEFLAG(Reserved, reserved) __CLEARPAGEFLAG(Reserved, reserved)
 PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
+#ifdef CONFIG_KSM_CHECK_PAGE
+CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
+CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
+#endif
 #ifdef CONFIG_ZCACHE
 PAGEFLAG(WasActive, was_active)
 #else
