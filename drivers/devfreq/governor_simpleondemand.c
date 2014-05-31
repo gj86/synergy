2645b076ebd16e6e2aada56279a134c7ae3e099f
myfluxi <linflux@arcor.de>
2014-05-31 07:59:42 +0200

    PM: devfreq: Define tunables with a static scope
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- drivers/devfreq/governor_simpleondemand.c
+++ drivers/devfreq/governor_simpleondemand.c
@@ -25,8 +25,8 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 	struct devfreq_dev_status stat;
 	int err = df->profile->get_dev_status(df->dev.parent, &stat);
 	unsigned long long a, b;
-	unsigned int dfso_upthreshold = DFSO_UPTHRESHOLD;
-	unsigned int dfso_downdifferential = DFSO_DOWNDIFFERENCTIAL;
+	static unsigned int dfso_upthreshold = DFSO_UPTHRESHOLD;
+	static unsigned int dfso_downdifferential = DFSO_DOWNDIFFERENCTIAL;
 	struct devfreq_simple_ondemand_data *data = df->data;
 	unsigned long max = (df->max_freq) ? df->max_freq : UINT_MAX;
 	unsigned long min = (df->min_freq) ? df->min_freq : 0;
