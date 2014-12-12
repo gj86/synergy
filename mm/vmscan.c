5c936f87c0bd2ff9b1787552da65a07b5323696a
Yuanyuan Zhong <zyy@motorola.com>
2014-12-11 18:03:34 -0600

    mm: vmscan: do not count anon pages as reclaimable if swap is small
    When swap size is smaller than ram size, not all anon pages can be
    written to swap space. So do not count anon pages as reclaimable.
    This will increase the slab scan rate under memory pressure.
    
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    (cherry picked from commit bc0d09af782178758f17ca23bae8017cbf7dd933)
    Reviewed-on: http://gerrit.mot.com/697880
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    # Conflicts:
    #	mm/vmscan.c
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -198,11 +198,9 @@ unsigned long zone_reclaimable_pages(struct zone *zone)
 	nr = zone_page_state(zone, NR_ACTIVE_FILE) +
 	     zone_page_state(zone, NR_INACTIVE_FILE);
 
-#ifndef CONFIG_RUNTIME_COMPCACHE
-	if (get_nr_swap_pages() > 0)
+	if (get_nr_swap_pages() > 0 && total_swap_pages >= totalram_pages)
 		nr += zone_page_state(zone, NR_ACTIVE_ANON) +
 		      zone_page_state(zone, NR_INACTIVE_ANON);
-#endif /* CONFIG_RUNTIME_COMPCACHE */
 
 	return nr;
 }
