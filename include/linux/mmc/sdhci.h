24b35536148f6a8e88e798e50612dcf2691d0f94
Ritesh Harjani <riteshh@codeaurora.org>
2015-02-10 14:58:07 +0530

    msm: sdhci: Modified version of dynamic pm_qos changes
    Improve I/O performance for 8939 based targets.
    It support legacy qos framework for all other targets.
    
    This modified pm_qos design improves read/write performance
    with minimum impact on power.
    
    Conflicts:
    	drivers/mmc/host/sdhci-msm.c
    	Documentation/devicetree/bindings/mmc/sdhci-msm.txt
    
    Change-Id: Idca6416d1311e37ccb43e1349d3fa606924a6061
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    Signed-off-by: Bharat Pawar <bpawar@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -50,6 +50,18 @@ struct sdhci_trace_buffer {
 	atomic_t			wr_idx;
 };
 
+struct sdhci_host_qos {
+	unsigned int *cpu_dma_latency_us;
+	unsigned int cpu_dma_latency_tbl_sz;
+	struct pm_qos_request pm_qos_req_dma;
+};
+
+enum sdhci_host_qos_policy {
+	SDHCI_QOS_READ_WRITE,
+	SDHCI_QOS_READ,
+	SDHCI_QOS_WRITE,
+	SDHCI_QOS_MAX_POLICY,
+};
 
 struct sdhci_host {
 	/* Data set by hardware interface driver */
@@ -273,11 +285,13 @@ struct sdhci_host {
 #define SDHCI_TUNING_MODE_1	0
 	struct timer_list	tuning_timer;	/* Timer for tuning */
 
-	unsigned int *cpu_dma_latency_us;
-	unsigned int cpu_dma_latency_tbl_sz;
-	struct pm_qos_request pm_qos_req_dma;
+	struct sdhci_host_qos host_qos[SDHCI_QOS_MAX_POLICY];
+	enum sdhci_host_qos_policy last_qos_policy;
+
+	bool host_use_default_qos;
 	unsigned int pm_qos_timeout_us;         /* timeout for PM QoS request */
 	struct device_attribute pm_qos_tout;
+	struct delayed_work pm_qos_work;
 
 	struct sdhci_next next_data;
 	ktime_t data_start_time;
