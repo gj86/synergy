c17cd95dddb26b69e38a11ce9b7efa16533af8a0
Dorimanx <yuri@bynet.co.il>
2015-07-07 18:35:21 +0300

    Sync kernel/workqueue.c with 3.10.y
    Conflicts:
    	include/linux/workqueue.h
    
    # Conflicts:
    #	kernel/workqueue.c
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -41,14 +41,13 @@
 #include <linux/debug_locks.h>
 #include <linux/lockdep.h>
 #include <linux/idr.h>
-#include <linux/hashtable.h>
-#include <linux/bug.h>
-#include <linux/moduleparam.h>
 #include <linux/jhash.h>
 #include <linux/hashtable.h>
 #include <linux/rculist.h>
 #include <linux/nodemask.h>
 #include <linux/moduleparam.h>
+#include <linux/uaccess.h>
+#include <linux/bug.h>
 
 #include "workqueue_internal.h"
 
@@ -286,6 +285,15 @@ static cpumask_var_t *wq_numa_possible_cpumask;
 
 static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
+
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0644);
 
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
@@ -5068,7 +5076,8 @@ static int __init init_workqueues(void)
 					      0);
 	BUG_ON(!system_wq || !system_highpri_wq || !system_long_wq ||
 	       !system_unbound_wq || !system_freezable_wq ||
-	       !system_power_efficient_wq || !system_freezable_power_efficient_wq);
+	       !system_power_efficient_wq ||
+	       !system_freezable_power_efficient_wq);
 	return 0;
 }
 early_initcall(init_workqueues);
