69a68cd4c74f81818f75a0466ea1a2b4633a1324
Dorimanx <yuri@bynet.co.il>
2015-08-16 02:36:07 +0300

    MM: fix mm/vmpressure.c

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -26,6 +26,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/vmpressure.h>
+#include <linux/slab.h>
 
 /*
  * The amount of windows we need to see for each pressure level before
