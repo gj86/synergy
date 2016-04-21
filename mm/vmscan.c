3c3d6377893c7e2b12aaa09d0f73fbf40ab42e5e
Dorimanx <yuri@bynet.co.il>
2016-04-21 12:28:05 +0300

    MM: add missing code from commit shrinker: add node awareness https://github.com/neobuddy89/shamu/commit/ca9dcb186c908e8f09e8cec869cf106e3b753123

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3232,6 +3232,8 @@ static bool kswapd_shrink_zone(struct zone *zone,
 		return true;
 
 	shrink_zone(zone, sc);
+	nodes_clear(shrink.nodes_to_scan);
+	node_set(zone_to_nid(zone), shrink.nodes_to_scan);
 
 	reclaim_state->reclaimed_slab = 0;
 	nr_slab = shrink_slab(&shrink, sc->nr_scanned, lru_pages);
