eba21c0d7250029663b82e092891faf5a699132c
Dorimanx <yuri@bynet.co.il>
2016-11-27 11:31:16 +0200

    ASYNC_FSYNC: exclude F2FS data partition from fast ASYNC_FSYNC
    users reported that with latest F2FS driver updates, driver crash on do_fsync
    
    if DATA is converted to F2FS partition, no problems with ext4.
    
    This should fix the problem.
    
    # Conflicts:
    #	fs/namespace.c
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1949,11 +1949,11 @@ static int do_new_mount(struct path *path, const char *fstype, int flags,
 	if (err)
 		mntput(mnt);
 #ifdef CONFIG_ASYNC_FSYNC
-	if (!err && ((!strcmp(fstype, "ext4") &&
-		!strcmp(path->dentry->d_name.name, "data")) ||
-		(!strcmp(fstype, "fuse") &&
-		!strcmp(path->dentry->d_name.name, "emulated"))))
-				mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
+	if (!err && ((!strcmp(fstype, "ext4") && (!strcmp(fstype, "f2fs") &&
+	    !strcmp(path->dentry->d_name.name, "data")) ||
+	    (!strcmp(fstype, "fuse") &&
+	    !strcmp(path->dentry->d_name.name, "emulated"))))
+                mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
 #endif
 	return err;
 }
