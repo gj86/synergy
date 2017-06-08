f5c0a77ffa2d45a87805abece0adddd099433b4a
Gobinda Joy <gobinda.joy@gmail.com>
2017-06-08 18:16:48 +0600

    Fix Phantom Governor

--- drivers/cpufreq/cpufreq_phantom.c
+++ drivers/cpufreq/cpufreq_phantom.c
@@ -146,6 +146,40 @@ static unsigned int up_threshold_any_cpu_load = DEFAULT_UP_THRESHOLD_LOAD;
 static unsigned int sync_freq = DEFAULT_SYNC_FREQ;
 static unsigned int up_threshold_any_cpu_freq = DEFAULT_UP_THRESHOLD_FREQ;
 
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+{
+	u64 idle_time;
+	u64 cur_wall_time;
+	u64 busy_time;
+
+	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+
+	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+
+	idle_time = cur_wall_time - busy_time;
+	if (wall)
+		*wall = jiffies_to_usecs(cur_wall_time);
+
+	return jiffies_to_usecs(idle_time);
+}
+
+static inline u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
+{
+	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
+
+	if (idle_time == -1ULL)
+		return get_cpu_idle_time_jiffy(cpu, wall);
+	else if (!io_busy)
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+	return idle_time;
+}
+
 static void cpufreq_phantom_timer_resched(
 	struct cpufreq_phantom_cpuinfo *pcpu)
 {
@@ -617,7 +651,7 @@ static int cpufreq_phantom_speedchange_task(void *data)
 				if (pjcpu->target_freq > max_freq)
 					max_freq = pjcpu->target_freq;
 			}
-						
+			
 			if (power_suspended && max_freq > screen_off_max_frequency)
 					max_freq = screen_off_max_frequency;
 
@@ -1421,6 +1455,7 @@ static int __init cpufreq_phantom_init(void)
 
 	/* NB: wake up so the thread does not look hung to the freezer */
 	wake_up_process(speedchange_task);
+
 	return cpufreq_register_governor(&cpufreq_gov_phantom);
 }
 
