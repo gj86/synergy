35bd93a8968e4b0c42c7e304616c5d2c8b48392d
Gobinda Joy <gobinda.joy@gmail.com>
2017-06-19 11:18:53 +0600

    driver:devfreq Update msm_adreno_tz governor
    Prepare for Smart Manager addition.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -19,6 +19,7 @@
 #include <linux/io.h>
 #include <linux/ftrace.h>
 #include <linux/msm_adreno_devfreq.h>
+#include <linux/powersuspend.h>
 #include <mach/scm.h>
 #include "governor.h"
 
@@ -28,11 +29,23 @@ static DEFINE_SPINLOCK(tz_lock);
  * FLOOR is 5msec to capture up to 3 re-draws
  * per frame for 60fps content.
  */
-#define FLOOR			5000
+#define FLOOR		        5000
+/*
+ * MIN_BUSY is 1 msec for the sample to be sent
+ */
+#define MIN_BUSY		1000
 #define LONG_FLOOR		50000
 #define HIST			5
 #define TARGET			80
 #define CAP			75
+
+/*
+ * Use BUSY_BIN to check for fully busy rendering
+ * intervals that may need early intervention when
+ * seen with LONG_FRAME lengths
+ */
+#define BUSY_BIN		95
+#define LONG_FRAME		25000
 
 /*
  * CEILING is 50msec, larger than any standard
@@ -44,6 +57,11 @@ static DEFINE_SPINLOCK(tz_lock);
 #define TZ_INIT_ID		0x6
 
 #define TAG "msm_adreno_tz: "
+
+/* Boolean to detect if pm has entered suspend mode */
+extern bool cpufreq_screen_on;
+static bool suspended = false;
+static bool power_suspended = false;
 
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
@@ -87,23 +105,40 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
 	struct devfreq_dev_status stats;
 	struct xstats b;
-	int val, level = 0;
+	int val = 0, level = 0, min_level = 0, max_level = 0;
 	int act_level;
 	int norm_cycles;
 	int gpu_percent;
+	static int busy_bin, frame_flag;
 
 	if (priv->bus.num)
 		stats.private_data = &b;
 	else
 		stats.private_data = NULL;
+
 	result = devfreq->profile->get_dev_status(devfreq->dev.parent, &stats);
 	if (result) {
 		pr_err(TAG "get_status failed %d\n", result);
 		return result;
 	}
 
+	/* Prevent overflow */
+	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
+		stats.busy_time >>= 7;
+		stats.total_time >>= 7;
+	}
+
 	*freq = stats.current_frequency;
 	*flag = 0;
+	/*
+	 * Force to use & record as min freq when system has
+	 * entered pm-suspend or screen-off state.
+	 */
+	if (suspended || power_suspended || !cpufreq_screen_on) {
+		*freq = devfreq->min_freq;
+		return 0;
+	}
+
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
 	if (priv->bus.num) {
@@ -116,15 +151,27 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	/*
 	 * Do not waste CPU cycles running this algorithm if
 	 * the GPU just started, or if less than FLOOR time
-	 * has passed since the last run.
+	 * has passed since the last run or the gpu hasn't been
+	 * busier than MIN_BUSY.
 	 */
 	if ((stats.total_time == 0) ||
-		(priv->bin.total_time < FLOOR)) {
-		return 1;
+		(priv->bin.total_time < FLOOR) ||
+		(unsigned int) priv->bin.busy_time < MIN_BUSY) {
+		return 0;
+	}
+
+	if ((stats.busy_time * 100 / stats.total_time) > BUSY_BIN) {
+		busy_bin += stats.busy_time;
+		if (stats.total_time > LONG_FRAME)
+			frame_flag = 1;
+	} else {
+		busy_bin = 0;
+		frame_flag = 0;
 	}
 
 	level = devfreq_get_freq_level(devfreq, stats.current_frequency);
-
+	min_level = devfreq->profile->max_state - 1;
+	max_level = 0;
 	if (level < 0) {
 		pr_err(TAG "bad freq %ld\n", stats.current_frequency);
 		return level;
@@ -134,8 +181,11 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	 * If there is an extended block of busy processing,
 	 * increase frequency.  Otherwise run the normal algorithm.
 	 */
-	if (priv->bin.busy_time > CEILING) {
-		val = -1 * level;
+	if (priv->bin.busy_time > CEILING ||
+		(busy_bin > CEILING && frame_flag)) {
+		val = -1 * (level - max_level);
+		busy_bin = 0;
+		frame_flag = 0;
 	} else {
 		val = __secure_tz_entry3(TZ_UPDATE_ID,
 				level,
@@ -151,11 +201,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	 */
 	if (val) {
 		level += val;
-		level = max(level, 0);
-		level = min_t(int, level, devfreq->profile->max_state);
+		level = max_t(int, level, max_level);
+		level = min_t(int, level, min_level);
 		goto clear;
 	}
-
 	if (priv->bus.total_time < LONG_FLOOR)
 		goto end;
 	norm_cycles = (unsigned int)priv->bus.ram_time /
@@ -174,6 +223,8 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		/*
 		 * Normalize by gpu_time unless it is a small fraction of
 		 * the total time interval.
+		 *
+		 * GPU votes for IB not AB so don't under vote the system
 		 */
 		norm_cycles = (100 * norm_cycles) / TARGET;
 		act_level = priv->bus.index[level] + b.mod;
@@ -285,13 +336,15 @@ static int tz_resume(struct devfreq *devfreq)
 
 	freq = profile->initial_freq;
 
+	suspended = false;
+
 	return profile->target(devfreq->dev.parent, &freq, 0);
 }
 
 static int tz_suspend(struct devfreq *devfreq)
 {
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
-
+	suspended = true;
 	__secure_tz_entry2(TZ_RESET_ID, 0, 0);
 
 	priv->bin.total_time = 0;
@@ -340,8 +393,26 @@ static struct devfreq_governor msm_adreno_tz = {
 	.event_handler = tz_handler,
 };
 
+static void tz_early_suspend(struct power_suspend *handler)
+{
+	power_suspended = true;
+	return;
+}
+
+static void tz_late_resume(struct power_suspend *handler)
+{
+	power_suspended = false;
+	return;
+}
+
+static struct power_suspend tz_power_suspend = {
+	.suspend = tz_early_suspend,
+	.resume = tz_late_resume,
+};
+
 static int __init msm_adreno_tz_init(void)
 {
+	register_power_suspend(&tz_power_suspend);
 	return devfreq_add_governor(&msm_adreno_tz);
 }
 subsys_initcall(msm_adreno_tz_init);
