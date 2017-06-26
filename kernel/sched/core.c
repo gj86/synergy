a8c620acfbbc4943be8f7264eb8ef55c4b82dc56
Gobinda Joy <gobinda.joy@gmail.com>
2017-06-26 20:05:33 +0600

    sched: cpufreq: update power usage only if cpufreq_stat is enabled
    Call acct_update_power() to track power usage of task only if
    CONFIG_CPU_FREQ_STAT is enabled, otherwise we run into
    following build failure:
    ---------------
    kernel/built-in.o: In function `account_user_time':
    kernel/sched/cputime.c:155: undefined reference to `acct_update_power'
    kernel/built-in.o: In function `__account_system_time':
    kernel/sched/cputime.c:208: undefined reference to `acct_update_power'
    make: *** [vmlinux] Error 1
    ---------------
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3006,8 +3006,10 @@ void account_user_time(struct task_struct *p, cputime_t cputime,
 	/* Account for user time used */
 	acct_update_integrals(p);
 
+#ifdef CONFIG_CPU_FREQ_STAT
 	/* Account power usage for user time */
 	acct_update_power(p, cputime);
+#endif
 }
 
 /*
@@ -3059,8 +3061,10 @@ void __account_system_time(struct task_struct *p, cputime_t cputime,
 	/* Account for system time used */
 	acct_update_integrals(p);
 
+#ifdef CONFIG_CPU_FREQ_STAT
 	/* Account power usage for system time */
 	acct_update_power(p, cputime);
+#endif
 }
 
 /*
