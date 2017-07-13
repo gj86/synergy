7018333c22dd95089537f3bc9ae2f356616b32b5
Gobinda Joy <gobinda.joy@gmail.com>
2017-07-14 04:57:57 +0600

    Revert "fs: Partially Revert "kernel: Only expose su when daemon is running""
    Magisk 13.2 sets selinux context properly now making this commit
    7c46baa5b3c9f8d36417ed8e3f9ca30da6c5a303 obsolete.
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1604,7 +1604,7 @@ static int do_execve_common(const char *filename,
 	if (retval < 0)
 		goto out;
 
-	if (d_is_su(file->f_dentry)) {
+	if (d_is_su(file->f_dentry) && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
