2df17c14291cbfbe251e30314bd1a182277453ab
Gobinda Joy <gobinda.joy@gmail.com>
2017-07-28 04:47:56 +0600

    mm: Enable KSM and and support for UKSM (patch 0.1.2.3)
    Update UKMS code to compile with Linux 3.4.110

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -734,6 +734,9 @@ const char * const vmstat_text[] = {
 	"nr_cma_unevictable",
 #endif
 	"nr_swapcache",
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
