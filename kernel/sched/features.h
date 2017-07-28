8cb62e286a1b24043cd34411367d6a09efa1aaf2
Gobinda Joy <gobinda.joy@gmail.com>
2017-07-28 14:55:47 +0600

    sched: rt: disable rt_runtime borrowing & gentle_fair_sleepers by default
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -3,7 +3,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Place new tasks ahead so that they do not starve already running
@@ -67,5 +67,5 @@ SCHED_FEAT(NONTASK_POWER, true)
 SCHED_FEAT(TTWU_QUEUE, true)
 
 SCHED_FEAT(FORCE_SD_OVERLAP, false)
-SCHED_FEAT(RT_RUNTIME_SHARE, true)
+SCHED_FEAT(RT_RUNTIME_SHARE, false)
 SCHED_FEAT(LB_MIN, false)
