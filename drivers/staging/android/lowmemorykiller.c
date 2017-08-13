80d7d47a1ac2d10100a80728349e1aa2e3d5e01a
Gobinda Joy <gobinda.joy@gmail.com>
2017-08-13 13:49:28 +0600

    Squashed Commits
    mm, oom: make dump_tasks public:
    Allow other functions to dump the list of tasks.
    Useful for when debugging memory leaks.
    
    Bug: 17871993
    Change-Id: I76c33a118a9765b4c2276e8c76de36399c78dbf6
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>
    --------------------------------
    
    lowmemorykiller: Run the lowmemory notifier when killing:
    When the lowmemory killer starts killing processes, it's useful to
    be able to see the state of other memory in the system. Call the
    lowmemory notifier to get other clients to dump out their state.
    
    Change-Id: Ie3b1d47149f0108b42b7961fd47a8cc5efe2c590
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    --------------------------------
    
    lowmemorykiller: Dump out slab state information:
    Leaks from the slab allocator are a very common cause of
    out of memory issues and excessive lowmemorykiller killing.
    Dump out the slab statistics as well to help with debugging.
    
    Change-Id: Ia5e4eeb66092c76f2b761321c88fa75c4138927c
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    --------------------------------
    
    lowmemorykiller: avoid false adaptive LMK triggers:
    In vmpressure notifier of LMK, shift_adj would have been set
    by a previous invocation of notifier, which is not followed by
    a lowmem_shrink yet. Since vmpressure has improved, reset
    shift_adj to avoid false adaptive LMK trigger.
    
    Change-Id: I2d77103d7c8f4d8a66e4652cba78e619a7bcef9a
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -48,6 +48,7 @@
 #include <linux/show_mem_notifier.h>
 #include <linux/vmpressure.h>
 #include <linux/zcache.h>
+#include <linux/show_mem_notifier.h>
 #include <linux/vmpressure.h>
 
 #define CREATE_TRACE_POINTS
@@ -236,7 +237,7 @@ int adjust_minadj(short *min_score_adj)
 static int lmk_vmpressure_notifier(struct notifier_block *nb,
 			unsigned long action, void *data)
 {
-	int other_free, other_file;
+	int other_free = 0, other_file = 0;
 	unsigned long pressure = action;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 
@@ -269,6 +270,15 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 				trace_almk_vmpressure(pressure, other_free,
 					other_file);
 		}
+	} else if (atomic_read(&shift_adj)) {
+		/*
+		 * shift_adj would have been set by a previous invocation
+		 * of notifier, which is not followed by a lowmem_shrink yet.
+		 * Since vmpressure has improved, reset shift_adj to avoid
+		 * false adaptive LMK trigger.
+		 */
+		trace_almk_vmpressure(pressure, other_free, other_file);
+		atomic_set(&shift_adj, 0);
 	}
 
 	return 0;
@@ -581,12 +591,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		selected = p;
 		selected_tasksize = tasksize;
 		selected_oom_score_adj = oom_score_adj;
-		lowmem_print(3, "select '%s' (%d), adj %hd, size %d, to kill\n",
+		lowmem_print(3, "select '%s' (%d), adj %d, size %d, to kill\n",
 			     p->comm, p->pid, oom_score_adj, tasksize);
 	}
 	if (selected) {
 		lowmem_print(1, "Killing '%s' (%d), adj %d,\n" \
-				"   to free %ldkB on behalf of '%s' (%d) because\n"
+				"   to free %ldkB on behalf of '%s' (%d) because\n" \
 				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n" \
 				"   Free memory is %ldkB above reserved.\n" \
 				"   Free CMA is %ldkB\n" \
@@ -598,31 +608,31 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 				"   Slab UnReclaimable is %ldkB\n" \
 				"   Total Slab is %ldkB\n" \
 				"   GFP mask is 0x%x\n",
-				selected->comm, selected->pid,
-				selected_oom_score_adj,
-				selected_tasksize * (long)(PAGE_SIZE / 1024),
-				current->comm, current->pid,
-				other_file * (long)(PAGE_SIZE / 1024),
-				minfree * (long)(PAGE_SIZE / 1024),
-				min_score_adj,
-				other_free * (long)(PAGE_SIZE / 1024),
-				global_page_state(NR_FREE_CMA_PAGES) *
-				   (long)(PAGE_SIZE / 1024),
-				totalreserve_pages * (long)(PAGE_SIZE / 1024),
-				global_page_state(NR_FREE_PAGES) *
-				   (long)(PAGE_SIZE / 1024),
-				global_page_state(NR_FILE_PAGES) *
-				   (long)(PAGE_SIZE / 1024),
+			     selected->comm, selected->pid,
+			     selected_oom_score_adj,
+			     selected_tasksize * (long)(PAGE_SIZE / 1024),
+			     current->comm, current->pid,
+			     other_file * (long)(PAGE_SIZE / 1024),
+			     minfree * (long)(PAGE_SIZE / 1024),
+			     min_score_adj,
+			     other_free * (long)(PAGE_SIZE / 1024),
+			     global_page_state(NR_FREE_CMA_PAGES) *
+				(long)(PAGE_SIZE / 1024),
+			     totalreserve_pages * (long)(PAGE_SIZE / 1024),
+			     global_page_state(NR_FREE_PAGES) *
+				(long)(PAGE_SIZE / 1024),
+			     global_page_state(NR_FILE_PAGES) *
+				(long)(PAGE_SIZE / 1024),
 				(long)zcache_pages() * (long)(PAGE_SIZE / 1024),
 				global_page_state(NR_SLAB_RECLAIMABLE) *
-				   (long)(PAGE_SIZE / 1024),
-				global_page_state(NR_SLAB_UNRECLAIMABLE) *
-				   (long)(PAGE_SIZE / 1024),
-				global_page_state(NR_SLAB_RECLAIMABLE) *
-				   (long)(PAGE_SIZE / 1024) +
-				global_page_state(NR_SLAB_UNRECLAIMABLE) *
-				   (long)(PAGE_SIZE / 1024),
-				sc->gfp_mask);
+				(long)(PAGE_SIZE / 1024),
+			     global_page_state(NR_SLAB_UNRECLAIMABLE) *
+				(long)(PAGE_SIZE / 1024),
+			     global_page_state(NR_SLAB_RECLAIMABLE) *
+				(long)(PAGE_SIZE / 1024) +
+			     global_page_state(NR_SLAB_UNRECLAIMABLE) *
+				(long)(PAGE_SIZE / 1024),
+			     sc->gfp_mask);
 
 		if (lowmem_debug_level >= 2 && selected_oom_score_adj == 0) {
 			show_mem(SHOW_MEM_FILTER_NODES);
