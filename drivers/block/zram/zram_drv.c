5ecf748c8279bb313ecd712b79b40746003b72f1
Gobinda Joy <gobinda.joy@gmail.com>
2017-09-04 03:49:43 +0600

    Revert "drivers: block: Add ZRam device"
    This reverts commit 908fa72d2d7f1a6d63dfd595a5df624199d081da.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -249,7 +249,7 @@ static struct zram_meta *zram_meta_alloc(u64 disksize)
 		goto free_meta;
 	}
 
-	meta->mem_pool = zs_create_pool(GFP_NOIO | __GFP_HIGHMEM, NULL);
+	meta->mem_pool = zs_create_pool(GFP_NOIO | __GFP_HIGHMEM);
 	if (!meta->mem_pool) {
 		pr_err("Error creating memory pool\n");
 		goto free_table;
@@ -490,9 +490,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 			src = uncmem;
 	}
 
-	handle = zs_malloc(meta->mem_pool, clen,
-		__GFP_NORETRY | __GFP_HIGHMEM | __GFP_NOMEMALLOC |
-			__GFP_NOWARN);
+	handle = zs_malloc(meta->mem_pool, clen);
 
 	if (!handle) {
 		pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
