0532c33d601cc5cf703922be160590d9bc4c0765
Gobinda Joy <gobinda.joy@gmail.com>
2020-04-29 10:08:55 +0600

    Merge updates
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -959,7 +959,6 @@ int try_to_del_timer_sync(struct timer_list *timer)
 	base = lock_timer_base(timer, &flags);
 
 	if (base->running_timer != timer) {
-		timer_stats_timer_clear_start_info(timer);
 		ret = detach_if_pending(timer, base, true);
 	}
 	spin_unlock_irqrestore(&base->lock, flags);
