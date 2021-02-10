834333246487ede074368f2b95980055091a2930
Gobinda Joy <gobinda.joy@gmail.com>
2021-02-10 21:05:01 +0600

    Enable by default with level 1 boost
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- sound/soc/codecs/boeffla_sound.h
+++ sound/soc/codecs/boeffla_sound.h
@@ -45,7 +45,7 @@ unsigned int boeffla_sound_hook_taiko_write(unsigned int reg, unsigned int value
 /*****************************************/
 
 // Boeffla sound general
-#define BOEFFLA_SOUND_DEFAULT 	0
+#define BOEFFLA_SOUND_DEFAULT 	1
 #define BOEFFLA_SOUND_VERSION 	"1.0.0"
 #define DEBUG_DEFAULT 			0
 
@@ -56,7 +56,7 @@ unsigned int boeffla_sound_hook_taiko_write(unsigned int reg, unsigned int value
 #define HEADPHONE_MAX 			30
 
 // speaker levels
-#define SPEAKER_DEFAULT			0
+#define SPEAKER_DEFAULT			1
 #define SPEAKER_REG_OFFSET		-4
 #define SPEAKER_MIN 			-30
 #define SPEAKER_MAX 			30
