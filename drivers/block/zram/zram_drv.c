862a75b20c7331242d417fb0c6673f1f79f97a1d
Gobinda Joy <gobinda.joy@gmail.com>
2021-04-23 23:59:45 +0600

    zram: restrict add/remove attributes to root only
    commit 5c7e9ccd91b90d87029261f8856294ee51934cab upstream.
    
    zram hot_add sysfs attribute is a very 'special' attribute - reading
    from it creates a new uninitialized zram device.  This file, by a
    mistake, can be read by a 'normal' user at the moment, while only root
    must be able to create a new zram device, therefore hot_add attribute
    must have S_IRUSR mode, not S_IRUGO.
    
    [akpm@linux-foundation.org: s/sence/sense/, reflow comment to use 80 cols]
    Fixes: 6566d1a ("zram: add dynamic device add/remove functionality")
    Link: http://lkml.kernel.org/r/20161205155845.20129-1-sergey.senozhatsky@gmail.com
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Reported-by: Steven Allen <steven@stebalien.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1335,8 +1335,14 @@ static ssize_t hot_remove_store(struct class *class,
 	return ret ? ret : count;
 }
 
+/*
+ * NOTE: hot_add attribute is not the usual read-only sysfs attribute. In a
+ * sense that reading from this file does alter the state of your system -- it
+ * creates a new un-initialized zram device and returns back this device's
+ * device_id (or an error code if it fails to create a new device).
+ */
 static struct class_attribute zram_control_class_attrs[] = {
-	__ATTR(hot_add, S_IRUGO, hot_add_show, NULL),
+	__ATTR(hot_add, 0400, hot_add_show, NULL),
 	__ATTR(hot_remove, S_IWUSR, NULL, hot_remove_store),
 	__ATTR_NULL,
 };
