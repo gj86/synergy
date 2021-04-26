1603a1a8daa3e32f68dd3313cc4ec9826df2886e
Gobinda Joy <gobinda.joy@gmail.com>
2021-04-26 23:32:09 +0600

    drop bogus check in file_open_root()
    For one thing, LOOKUP_DIRECTORY will be dealt with in do_last().
    For another, name can be an empty string, but not NULL - no callers
    pass that and it would oops immediately if they would.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -985,9 +985,6 @@ struct file *file_open_root(struct dentry *dentry, struct vfsmount *mnt,
 	int lookup = build_open_flags(flags, 0, &op);
 	if (flags & O_CREAT)
 		return ERR_PTR(-EINVAL);
-	if (!filename && (flags & O_DIRECTORY))
-		if (!dentry->d_inode->i_op->lookup)
-			return ERR_PTR(-ENOTDIR);
 	return do_file_open_root(dentry, mnt, filename, &op, lookup);
 }
 EXPORT_SYMBOL(file_open_root);
